8097157a1a37f2e173766417573947c37e93525f
biraj-phuyal <birajphuyal11@gmail.com>
2025-08-24 23:32:40 +0100

    add

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,12 +16,13 @@ const Hero = () => {
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-        <h1 className='bold-52 lg:bold-88'> Putuk Turno Camp Area</h1>
-        <p className='regular-16 mt-6 text-gray-500 xl:max-w-[-520px]'>
+        <h1 className='bold-52 lg:bold-88 xl:max-w-[600px]'>Putuk Turno Camp Area</h1>
+        <p className='regular-16 mt-6 text-gray-500 xl:max-w-[520px]'>
           We want to be on each of your journeys seeking the 
           satisfaction of seeing the incorruptible beauty of nature.
           We can help you on an adventure around the world in just one app
         </p>
+
         <div className='my-11 flex flex-wrap gap-5'>
           <div className='flex items-center gap-2'>
               {Array(5).fill(1).map((_, index) => (
@@ -35,11 +36,18 @@ const Hero = () => {
               ))}
 
             <p className='bold-16 lg:bold-20 text-blue-400'> 198k
-              <span className='regular-16 lg:regular-20 ml-1'>Excellent Reviews</span>
+              <span className='regular-16 
+                  lg:regular-20 ml-1'>Excellent Reviews</span>
             </p>
 
-            <div className=''>
-              <Button 
+            <div className='flex w-full gap-3 sm:flex-row'>
+              <Button
+                type='button'
+                title='Download App'
+                variant='btn_green'
+              />
+
+              <Button
                 type='button'
                 title='How we work?'
                 icon='/play.svg'
@@ -48,32 +56,33 @@ const Hero = () => {
             </div>
           </div>
 
-            <div className='relative flex flex-1 item-start'>
-              <div className='relative z-20 w-[268px] flex-col gap-8
-              rounded-3xl bg-black px-7 py-8'>
+          <div className='relative flex flex-1 item-start'>
+            <div className='relative z-20 flex w-[268px] flex-col gap-8
+                  rounded-3xl bg-black px-7 py-8'>
 
 
-                <div className='flex flex-col'>
-                  <div className='flexBetween'>
-                    <p className='regular-16 text-gray-50'> Location </p>
-                    <Image src='/close.svg' alt='close' width={24} height={24} />
-                  </div>
-                  <p className='bold-20 text-white'> Augas Caliente</p>
-                </div>
-
-
+              <div className='flex flex-col'>
                 <div className='flexBetween'>
-                    <div className='felx felx-col'>
-                      <p className='regular-16 block text-gray-50'>Distance</p>
-                      <p className='bold-20 text-white'> 173.28 </p>
-                    </div>
-                  <div className='felx felx-col'>
-                      <p className='regular-16 block text-gray-50'>Elevation</p>
-                      <p className='bold-20 text-white'> 2.040 km </p>
-                    </div>
+                  <p className='regular-16 text-gray-50'>Location</p>
+                  <Image src='/close.svg' alt='close' width={24} height={24} />
                 </div>
+                <p className='bold-20 text-white'>Augas Caliente</p>
+              </div>
+
+
+              <div className='flexBetween'>
+                  <div className='flex flex-col'>
+                    <p className='regular-16 block text-gray-50'>Distance</p>
+                    <p className='bold-20 text-white'> 173.28 </p>
+                  </div>
+                  <div className='flex felx-col'>
+                    <p className='regular-16 block text-gray-50'>Elevation</p>
+                    <p className='bold-20 text-white'> 2.040 km </p>
+                  </div>
+              </div>
             </div>
           </div>
+        </div>
       </div>
     </section>
   )
