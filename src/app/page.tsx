24fc25679cef45bcc8ddc758a155fbcf8b49009e
biraj-phuyal <birajphuyal11@gmail.com>
2025-09-01 16:34:19 +0100

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Camp from "@/components/Camp";
 import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
 
 export default function home() {
@@ -8,6 +9,7 @@ export default function home() {
     <>
       <Hero />
       <Camp />
+      <Guide />
       <Features />
       <GetApp />
     </>
